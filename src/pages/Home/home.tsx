aeff20bbdf1f8a63569391125e054fd6bccd7394
Lucienzan <changwilliam826@gmail.com>
2024-06-22 01:43:28 +0630

    [main] #added search filter data

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -11,12 +11,23 @@ const Home: React.FC = () => {
 
   const navigate = useNavigate();
   const [originalData, setOriginalData] = useState<Resources[]>([]);
+  const [filteredData, setFilteredData] = useState<Resources[]>([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
     if (data != null) {
       setOriginalData(data);
     }
   }, [data]);
+
+  useEffect(() => {
+    const filtered = originalData.filter((item) => {
+      return searchQuery === ""
+        ? true
+        : item.title.toLowerCase().includes(searchQuery.toLowerCase());
+    });
+    setFilteredData(filtered);
+  }, [originalData, searchQuery]);
 
   if (error) {
     return (
@@ -25,31 +36,34 @@ const Home: React.FC = () => {
       </>
     );
   }
-  const showDetailPageHandler = (id: number) => {
+
+  const showDetailPageHandler = (id: string) => {
     navigate(`/post/detail/${id}`);
   };
+
+  const findItem = (text: string) => {
+    setSearchQuery(text.toLowerCase());
+  };
+
   return (
     <>
       <div className="max-w-screen-xl mx-auto p-4">
-        <Search />
+        <Search SearchItem={findItem} />
         <button
           type="button"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           onClick={() => navigate("post/create")}
-          >
+        >
           Create
         </button>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
-          {!!originalData &&
-            originalData.map((item) => {
-              return (
-                <Card
-                  key={item.id}
-                  data={item}
-                  linkTo={showDetailPageHandler}
-                />
-              );
-            })}
+          {!!filteredData && filteredData.length > 0 ? (
+            filteredData.map((item) => (
+              <Card key={item.id} data={item} linkTo={showDetailPageHandler} />
+            ))
+          ) : (
+            <h1>No data</h1>
+          )}
         </div>
       </div>
     </>
